35d24732a93a734c935a6563e92f683583e016e4
Steven Tran <steven85048@gmail.com>
2020-10-17 10:42:53 -0700

    Clean up frame_parser code a bit

--- video_utils/frame_parser.py
+++ video_utils/frame_parser.py
@@ -1,25 +1,33 @@
 import pafy
 import cv2
 
-IMAGE_IN = 'test-video.mp4'
-IMAGE_OUT = 'test_frames/'
+FRAME_CAPTURE_INTERVAL = 1000 # in ms
 
-test_youtube_vid = 'https://www.youtube.com/watch?v=DNMO-MdmO6g&ab_channel=HiroshiNakamura'
+def extract_frames_from_youtube(youtube_url, path_out):
+    vc = _vidcap_from_youtube(youtube_url)
+    _extract_image_frames(vc, path_out)
 
-def vidcap_from_youtube(youtube_url):
+def _vidcap_from_youtube(youtube_url):
     video = pafy.new(youtube_url)
     print(video.title)
     stream = video.getbest()
     return cv2.VideoCapture(stream.url)
 
-def extract_image_frames(vidcap, path_in, path_out):
+def _vidcap_from_local_video(vid_path):
+    return cv2.VideoCapture(vid_path)
+
+def _extract_image_frames(vidcap, path_out):
+    """
+    Extract frames from a video at FRAME_CAPTURE_INTERVAL milliseconds and
+    stores those frames into path_out.
+    """
     count = 1
 
     success,image = vidcap.read()
     success = True
 
     while success:
-        vidcap.set(cv2.CAP_PROP_POS_MSEC, (count * 10000))
+        vidcap.set(cv2.CAP_PROP_POS_MSEC, (count * FRAME_CAPTURE_INTERVAL))
 
         success,image = vidcap.read()
         if(success):
@@ -27,5 +35,8 @@ def extract_image_frames(vidcap, path_in, path_out):
 
         count = count + 1
 
-vc = vidcap_from_youtube(test_youtube_vid)
-extract_image_frames(vc, IMAGE_IN, IMAGE_OUT)
+if __name__ == "__main__":
+    path_out = 'test_frames/'
+    test_youtube_vid = 'https://www.youtube.com/watch?v=DNMO-MdmO6g&ab_channel=HiroshiNakamura'
+
+    extract_frames_from_youtube(test_youtube_vid, path_out)
